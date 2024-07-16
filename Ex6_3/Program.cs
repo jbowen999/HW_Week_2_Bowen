78e4f42f85f452f492100379ccdba9123a827b68
jbowen999 <julian.e.bowen@outlook.com>
2024-07-16 16:51:13 -0700

    Finished

---
+++ Ex6_3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ex6_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double number = 9.0;
+            double sqrt = SquareRoot(number);
+            Console.WriteLine($"Approximate square root of {number} is {sqrt}");
+        }
+
+        static double SquareRoot(double a)
+        {
+            if (a < 0)
+            {
+                throw new ArgumentException("Cannot compute square root of a negative number");
+            }
+
+            double x0 = a / 2;
+            double x1 = (x0 + a / x0) / 2;
+
+            while (Math.Abs(x1 - x0) >= 0.0001)
+            {
+                x0 = x1;
+                x1 = (x0 + a / x0) / 2;
+            }
+
+            return x1;
+        }
+    }
+}
